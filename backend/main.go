08ce9d81dad607ac74487b44eb8956994d13b865
semkufu95 <semkufu95@gmail.com>
2025-08-12 17:46:58 +0300

    CORS setup for render

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"log"
-	"os"
 
 	"github.com/Semkufu95/confessions/Backend/config"
 	"github.com/Semkufu95/confessions/Backend/routes"
@@ -17,7 +18,7 @@ func main() {
 
 	// Enable CORS for React frontend on port 3000
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "https://confessions-1-7uoh.onrender.com/",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 	}))
