20df7ea19193db977cb944b26cba9ca899fe3bf3
semkufu95 <semkufu95@gmail.com>
2025-12-16 23:54:46 +0300

    Configure vite server with websockets (wss)

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
     optimizeDeps: {
-      exclude: ['lucide-react'],
+        exclude: ['lucide-react'],
     },
     server: {
-      hmr: {
-          protocol: 'wss',
-          host: 'www.confessions.africa',
-          port: 443,
-      },
-      host: 'true',
-      allowedHosts: ['confessions.africa'],
+        host: true,
+        allowedHosts: [
+            'confessions.africa',
+            'www.confessions.africa',
+        ],
+        hmr: {
+            protocol: 'wss',
+            host: 'www.confessions.africa',
+            port: 443,
+        },
     },
 });
